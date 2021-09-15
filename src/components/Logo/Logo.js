50d14f6e0f6457b4bfac4414ea368100f9030df3
Jack Thomas <jackthomas6596@live.co.uk>
2021-09-15 16:38:20 +0100

    got the brain png and added to project

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tilt from 'react-tilt'
+import brain from './brain.png';
 import './Logo.css'
 
 
@@ -7,7 +8,7 @@ const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
 			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-	 			<div className="Tilt-inner"> 👽 </div>
+	 			<div className="Tilt-inner"> <img src={brain}> </div>
 			</Tilt>
 		</div>
 	);
