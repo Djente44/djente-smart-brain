fc2d1806a83a6587908cf3420822957e39441dfd
Jack Thomas <jackthomas6596@live.co.uk>
2021-09-16 10:52:59 +0100

    more styling and more functionality, started working on rank

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@
 import Navigation from './components/Navigation/Navigation.js'
 import Logo from './components/Logo/Logo.js'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
+import Rank from './components/Rank/Rank.js'
+import Particles from 'react-particles-js';
 import './App.css';
 
+
+// class App extends Component{
+  
+//     render(){
+//         return (
+//             
+//         );
+//     };
+
+// }
 
 function App() {
   return (
     <div className="App">
-    <Navigation />
-    <Logo />
-    <ImageLinkForm />
-       {/*{
-       <FaceRecognition /}*/}
+      <Particles />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+         {/*{
+         <FaceRecognition /}*/}
     </div>
   );
 }
