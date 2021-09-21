0df05db58d5fa4b12699401e1dba9eca12441565
Jack Thomas <jackthomas6596@live.co.uk>
2021-09-21 08:39:28 +0100

    newest set of working files before working on 21,9

--- src/App.js
+++ src/App.js
@@ -1,34 +1,93 @@
 
 import Navigation from './components/Navigation/Navigation.js'
 import Logo from './components/Logo/Logo.js'
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
 import Rank from './components/Rank/Rank.js'
 import Particles from 'react-particles-js';
+import React, { Component } from "react"
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: '809c00bce2c54d33a83e1557153452f6'
+});
 
-// class App extends Component{
-  
-//     render(){
-//         return (
-//             
-//         );
-//     };
 
-// }
-
-function App() {
-  return (
-    <div className="App">
-      <Particles />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-         {/*{
-         <FaceRecognition /}*/}
-    </div>
-  );
+const particleOptions = {
+  "interactivity": {
+          "events": {
+              "onhover": {
+                  "enable": true,
+                  "mode": "repulse"
+              }
+          }
+      },
+  particles: {
+     "size": {
+              "value": 3
+          },
+    collisions: {
+              enable: true
+          },
+   number: {
+    value:90,
+    density: {
+      enable:true,
+      value_area: 1500
+    }
+   }
+  }
 }
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
+  render (){
+    return (
+      <div className="App">
+        <Particles className='particles'
+        params={particleOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
+}
+
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className='f4'>
-				{'This Magic Brain will detect all the faces in your pictures... Why dont you try it?...'}
+				{'This Magic Brain will detect all the faces in your pictures...'} <br /> {'Why dont you try it?...'}
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center' type='text' />
-					<button className=' w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+					<button onClick={onButtonSubmit} 
+						className=' w-30 grow f4 link ph3 pv2 dib white bg-light-purple '
+						>
+						Detect
+					</button>
 				</div>
 			</div>
 		</div>
